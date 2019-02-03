59bf614954c82e46eb1ac49ff2f154c636b6550a
PHUNG <phung.duy@newunion.sg>
2019-02-03 18:21:10 +0700

    save start base project

--- Sources/App/Controllers/UsersController.swift
+++ Sources/App/Controllers/UsersController.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import Vapor
+// 1
+struct UsersController: RouteCollection {
+    // 2
+    func boot(router: Router) throws {
+        // 3
+        let usersRoute = router.grouped("api", "users")
+        // 4
+        usersRoute.post(User.self, use: createHandler)
+        usersRoute.get(use: getAllHandler)
+        
+    }
+    // 5
+    func createHandler(
+        _ req: Request,
+        user: User
+        ) throws -> Future<User> {
+        // 6
+        return user.save(on: req)
+    }
+    func getAllHandler(_ req: Request) throws -> Future<[User]> {
+        // 2
+        return User.query(on: req).all()
+    }
+    // 3
+    func getHandler(_ req: Request) throws -> Future<User> {
+        // 4
+        return try req.parameters.next(User.self)
+    }
+}
--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -5,4 +5,19 @@
 //  Created by phung on 2/3/19.
 //
 
-import Foundation
+import Vapor
+import FluentPostgreSQL
+
+final class User: Codable {
+    var id: UUID?
+    var name: String
+    var username: String
+    init(name: String, username: String) {
+        self.name = name
+        self.username = username
+    }
+}
+extension User: PostgreSQLUUIDModel {}
+extension User: Content {}
+extension User: Migration {}
+extension User: Parameter {}

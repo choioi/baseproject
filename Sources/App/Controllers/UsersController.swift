d29064f76dc93905cd6285b279dd543b1f598565
PHUNG <phung.duy@newunion.sg>
2019-02-04 01:43:39 +0700

    enrypt pass, anti dup username, hide password return

--- Sources/App/Controllers/UsersController.swift
+++ Sources/App/Controllers/UsersController.swift
@@ -1,38 +1,41 @@
-//
-//  UsersController.swift
-//  App
-//
-//  Created by phung on 2/3/19.
-//
-
 import Foundation
 import Vapor
-// 1
+import Crypto
+
 struct UsersController: RouteCollection {
-    // 2
     func boot(router: Router) throws {
-        // 3
         let usersRoute = router.grouped("api", "users")
-        // 4
         usersRoute.post(User.self, use: createHandler)
-        usersRoute.get(use: getAllHandler)
-        
+        usersRoute.get(User.parameter, use: getHandler)
+        usersRoute.delete(User.parameter, use: deleteHandler)
+        usersRoute.put(User.parameter, use: updateHandler)
     }
-    // 5
-    func createHandler(
-        _ req: Request,
-        user: User
-        ) throws -> Future<User> {
-        // 6
-        return user.save(on: req)
+    //ADD
+    func createHandler(_ req: Request, user: User) throws -> Future<User.Public> {
+        user.password = try BCrypt.hash(user.password)
+        return user.save(on: req).convertToPublic()
     }
+    //LIST ALL
     func getAllHandler(_ req: Request) throws -> Future<[User]> {
-        // 2
         return User.query(on: req).all()
     }
-    // 3
+    //LIST ONE
     func getHandler(_ req: Request) throws -> Future<User> {
-        // 4
         return try req.parameters.next(User.self)
     }
+    //DELETE
+    func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
+        return try req.parameters.next(User.self).delete(on: req).transform(to: HTTPStatus.noContent)
+    }
+    //EDIT
+    func updateHandler(_ req: Request) throws -> Future<User> {
+        return try flatMap(to: User.self,
+                           req.parameters.next(User.self),
+                           req.content.decode(User.self)) { user, updatedUser in
+                            user.password = updatedUser.password
+                            return user.save(on: req)
+        }
+    }
+    
 }
+
--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -12,12 +12,49 @@ final class User: Codable {
     var id: Int?
     var name: String
     var username: String
-    init(name: String, username: String) {
+    var password: String
+    init(name: String, username: String,password: String) {
         self.name = name
         self.username = username
+        self.password = password
     }
+    final class Public: Codable {
+        var id: Int?
+        var name: String
+        var username: String
+        
+        init(id: Int?, name: String, username: String) {
+            self.id = id
+            self.name = name
+            self.username = username
+        }
+    }
+   
 }
 extension User: PostgreSQLModel {}
 extension User: Content {}
-extension User: Migration {}
 extension User: Parameter {}
+extension User.Public: Content {}
+//Making usernames unique
+extension User: Migration {
+    static func prepare(on connection: PostgreSQLConnection)
+        -> Future<Void> {
+            return Database.create(self, on: connection) { builder in
+                try addProperties(to: builder)
+                builder.unique(on: \.username)
+            }
+    }
+}
+//2 ham nay di chung! Future 1 function
+extension User {
+    func convertToPublic() -> User.Public {
+        return User.Public(id: id, name: name, username: username)
+    }
+}
+extension Future where T: User {
+    func convertToPublic() -> Future<User.Public> {
+        return self.map(to: User.Public.self) { user in
+            return user.convertToPublic()
+        }
+    }
+}

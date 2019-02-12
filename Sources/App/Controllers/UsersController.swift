d904bb01008ba4b6c8628f25f60f0500b60214d4
PHUNG <phung.duy@newunion.sg>
2019-02-12 14:09:06 +0700

    ..

--- Sources/App/Controllers/UsersController.swift
+++ Sources/App/Controllers/UsersController.swift
@@ -7,10 +7,9 @@ import Fluent // ~~ phai import cai nay
 struct UsersController: RouteCollection {
     func boot(router: Router) throws {
         let usersRoute = router.grouped("api", "users")
-       //BAsic authen
-//        let basicAuthMiddleware = User(name: "demo", username: "demo", password: "123").basicAuthMiddleware(using: BCryptDigest())
-        
+
         let basicAuthMiddleware = User.basicAuthMiddleware(using: BCryptDigest())
+
         let tokenAuthMiddleware = User.tokenAuthMiddleware()
        
         let guardAuthMiddleware = User.guardAuthMiddleware()
@@ -30,28 +29,31 @@ struct UsersController: RouteCollection {
         
         
         //basic protect
-        basicProtected.post(User.self, at: "register", use: createHandler3)//Nen dung cai na`y... tich hoo san content ko can decode cac kieu~
-        basicProtected.post(User.self, use: createHandler)
-        //basicProtected.post(LoginParam.self, at: "login", use: login)
+        //add
+        basicProtected.post(User.self, at: "register", use: createHandler)
+        //login
         basicProtected.get("login", use: login)
-        basicProtected.post(TokenParam.self, at: "profile", use: profile)
-        
+        //get profile
+        tokenProtected.post("profile", use: getCurrentProfileHandler)
+        //update
+        tokenProtected.post(User.self, at: "update", use: updateHandler)
         
         //token protect
+        //get all user => for test or admin only
         tokenProtected.get(use: getAllHandler)
-        tokenProtected.get(User.parameter, use: getHandler)
-        tokenProtected.delete(User.parameter, use: deleteHandler)
-        tokenProtected.put(User.parameter, use: updateHandler)
-
-        tokenProtected.post(User.self, at: "update", use: updateHandler2)
+       
+        //Ko cho xoa user, xoa item cua user
+        //tokenProtected.delete(User.parameter, use: deleteHandler)
+        
 
         
     }
     //=================================ADD=================================
+    /*
     func createHandler(_ req: Request, user: User) throws -> Future<User.Public> {
         user.password = try BCrypt.hash(user.password)
         return user.save(on: req).convertToPublic()
-    }
+    }//==> ko nen dung, vi URL ko ro rang
     func createHandler2(_ req: Request) throws -> Future<User.Public> {
         return try req.content.decode(User.self).flatMap { user in
             let hasher = try req.make(BCryptDigest.self)
@@ -62,8 +64,9 @@ struct UsersController: RouteCollection {
                 return storedUser.convertToPublic()
             }
         }
-    }
-    func createHandler3(_ req: Request,user: User) throws -> Future<User.Public> {
+    }//=> ko nen dung vi phai decode content
+     */
+    func createHandler(_ req: Request,user: User) throws -> Future<User.Public> {
         user.password = try BCrypt.hash(user.password)//update raw password thanh hashed password
         return user.save(on: req).convertToPublic() // save xong vao database return model public!
     }
@@ -71,6 +74,7 @@ struct UsersController: RouteCollection {
    
     //EDIT
     //Ko nen dung cach nay, vi co the hack truyen 1 tham so kha'c
+    /*
     func updateHandler(_ req: Request) throws -> Future<User> {
         return try flatMap(to: User.self,
                            req.parameters.next(User.self),
@@ -86,7 +90,8 @@ struct UsersController: RouteCollection {
                             return user.save(on: req)
         }
     }
-    func updateHandler2(_ req: Request,user: User) throws -> Future<User> {
+     */
+    func updateHandler(_ req: Request,user: User) throws -> Future<User> {
         let userNeedEdit = try req.requireAuthenticated(User.self)
         userNeedEdit.name = user.name
         userNeedEdit.password = try BCrypt.hash(user.password)
@@ -109,20 +114,17 @@ struct UsersController: RouteCollection {
     func clearOldTokenHaveID(req : Request,userID: Int?) -> Future<HTTPStatus> {
         return Token.query(on: req).filter(\.userID == userID ?? 0).delete().transform(to: HTTPStatus.continue)
     }
-//    func login2(_ req: Request) throws -> User.Public {
-//        let user = try req.requireAuthenticated(User.self)
-//        return User.Public(id: try user.requireID(), name: user.name, email: user.email)
-//    }
+
     //=================================LOGIN=================================
     
     //=================================VIEW PROFILE=================================
-    func profile(_ req: Request,tokenParam: TokenParam) throws -> User {
+    func getCurrentProfileHandler(_ req: Request) throws -> User {
         let user = try req.requireAuthenticated(User.self)
         return user
     }
     
     
-    
+    /*
     func getToKenFromTokenString(req: Request,tokenString: String) -> Future<Token> {
         print("getToKenFromTokenString")
         return Token.query(on: req).filter(\.token == tokenString).first().unwrap(or: Abort(.notFound))
@@ -135,7 +137,7 @@ struct UsersController: RouteCollection {
         print("getTokenFromUserID:\(userID)")
         return Token.query(on: req).filter(\.userID == userID).first().unwrap(or: Abort(.notFound))
     }
-    
+    */
     //=================================VIEW PROFILE=================================
     
     
@@ -151,17 +153,8 @@ struct UsersController: RouteCollection {
     func getAllTkenHandler(_ req: Request) throws -> Future<[Token]> {
         return Token.query(on: req).all()
     }
+
     
-    
-//    func getAllHandler(_ req: Request) throws -> Future<[User.Public]> {
-//        return User.query(on: req).decode(data: User.Public.self).all()
-//    }
-//
-    //LIST ONE
-    func getHandler(_ req: Request) throws -> Future<User.Public> {
-       
-        return try req.parameters.next(User.self).convertToPublic()
-    }
     //DELETE
     func deleteHandler(_ req: Request) throws -> Future<HTTPStatus> {
         return try req.parameters.next(User.self).delete(on: req).transform(to: HTTPStatus.noContent)

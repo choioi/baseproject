164b39bc520dfeebeb1952acf937ebb94983e01b
PHUNG <phung.duy@newunion.sg>
2019-02-24 13:46:22 +0700

    update config heroku

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -30,18 +30,25 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     let username = Environment.get("DATABASE_USER") ?? "vapor"
     let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
     let password = Environment.get("DATABASE_PASSWORD") ?? "password"
-    /*
-    let databaseConfig = PostgreSQLDatabaseConfig(
-        hostname: hostname,
-        username: username,
-        database: databaseName,
-        password: password)
-    */
-    let databaseConfig = PostgreSQLDatabaseConfig(
-        hostname: "localhost",
-        username: "vapor",
-        database: "vapor",
-        password: "password")
+    
+    let databaseConfig: PostgreSQLDatabaseConfig
+    if let url = Environment.get("DATABASE_URL") {
+        databaseConfig = PostgreSQLDatabaseConfig(url: url)!
+    } else {
+        let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
+        let username = Environment.get("DATABASE_USER") ?? "vapor"
+        let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
+        let databasePort = 5432
+        let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+        
+        databaseConfig = PostgreSQLDatabaseConfig(
+            hostname: hostname,
+            port: databasePort,
+            username: username,
+            database: databaseName,
+            password: password)
+    }
+    
     
     let database = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .psql)

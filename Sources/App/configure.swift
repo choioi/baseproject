3683e2c67ccdf2a55e6e0e00fdf3ce7263aea265
PHUNG <phung.duy@newunion.sg>
2019-02-27 13:41:52 +0700

    setup bien moi truong

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -29,15 +29,33 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     
     
     let databaseConfig: PostgreSQLDatabaseConfig
+    
+    
+    
+    
     if let url = Environment.get("DATABASE_URL") {
         databaseConfig = PostgreSQLDatabaseConfig(url: url)!
     } else {
-        let hostname = Environment.get("DATABASE_HOSTNAME") ?? "localhost"
-        let username = Environment.get("DATABASE_USER") ?? "vapor"
-        let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
-        let databasePort = 5432
-        let password = Environment.get("DATABASE_PASSWORD") ?? "password"
+        //check LIVE
+        let LIVE:String = Environment.get("DATABASE_LIVE") ?? "false"
+        var hostname = "localhost"
+        var username = "vapor"
+        var databaseName = "vapor"
+        var databasePort = 5432
+        var password = "password"
         
+        if LIVE.lowercased() == "TRUE".lowercased() {
+            hostname = Environment.get("DATABASE_HOSTNAME") ?? hostname
+            username = Environment.get("DATABASE_USER") ?? username
+            databaseName = Environment.get("DATABASE_DB") ?? databaseName
+            password = Environment.get("DATABASE_PASSWORD") ?? password
+        } else {
+            hostname = Environment.get("DATABASE_HOSTNAME_DEV") ?? hostname
+            username = Environment.get("DATABASE_USER_DEV") ?? username
+            databaseName = Environment.get("DATABASE_DB_DEV") ?? databaseName
+            password = Environment.get("DATABASE_PASSWORD_DEV") ?? password
+        }
+
         databaseConfig = PostgreSQLDatabaseConfig(
             hostname: hostname,
             port: databasePort,
@@ -45,7 +63,22 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
             database: databaseName,
             password: password)
     }
-    
+    /*
+     environment=
+     DATABASE_HOSTNAME="dbinstances.cr8mipihdiyf.ap-southeast-1.rds.amazonaws.com",
+     DATABASE_USER="phung",
+     DATABASE_DB="dbname",
+     DATABASE_PASSWORD="Heo123456"
+     */
+    /*
+    //amazone
+    databaseConfig = PostgreSQLDatabaseConfig(
+        hostname: "dbinstances.cr8mipihdiyf.ap-southeast-1.rds.amazonaws.com",
+        port: 5432,
+        username: "phung",
+        database: "dbname",
+        password: "Heo123456")
+    */
     
     let database = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .psql)
